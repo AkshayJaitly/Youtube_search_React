74192cfc3b265cda85d662d14af6e9a247a145b9
AKSHAY JAITLY <akshay365@icloud.com>
2018-07-12 18:29:24 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
-const API_KEY = "AIzaSyDIl5V4Wr5bsh81ejOlLfFajwrwvUqFHLI";
+const API_KEY = "Your key here";
 
 class App extends Component {
   constructor(props) {
